ed7da842df7ca5bdb1737351453c1edf9bf08488
지형 <jihyoung.lee90@gmail.com>
2021-07-20 05:37:05 +0900

    Rename header to header_css
    블레이드파일 이름 변경

---
+++ resources/views/layout/header_css.blade.php
@@ -0,0 +1,15 @@
+<!-- font -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap" rel="stylesheet">
+
+<!--csrf-token-->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<!--stylesheet-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+
+
+
