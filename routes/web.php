eea56a9b562efe30d135c1227cdfe501fffe33f2
지형 <jihyoung.lee90@gmail.com>
2021-08-02 19:49:16 +0900

    Add Diseaselist url
    /list 세그먼트 수정

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DiseaseController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');});
 
 /* Disease list */
-Route::match(['post','get'],[DiseaseController::class, 'disease_list'])->name('disease.list');
+Route::match(['post','get'],'/list',[DiseaseController::class, 'disease_list'])->name('disease.list');
