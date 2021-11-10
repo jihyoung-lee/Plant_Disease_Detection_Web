65c713740d7bb3fd25cdd9029131e07c2e8d5074
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2021-11-10 14:06:47 +0900

    Update header_css.blade.php
    불필요한 코드 제거

--- resources/views/layout/header_css.blade.php
+++ resources/views/layout/header_css.blade.php
@@ -1,12 +1,7 @@
-<!-- font -->
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap" rel="stylesheet">
-
-<!--csrf-token-->
+<!-- Goggle Font -->
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
 <meta name="csrf-token" content="{{ csrf_token() }}">
-
-<!--stylesheet-->
+<!-- stylesheet -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
