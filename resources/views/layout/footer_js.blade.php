2ae4817fe1096f07da1f6989ef5813ccea5f4caf
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2021-12-30 11:52:00 +0900

    Create footer_js.blade.php

---
+++ resources/views/layout/footer_js.blade.php
@@ -0,0 +1,10 @@
+<!-- bootstrap js -->
+<script src="{{ asset('js/bootstrap.js') }}"></script>
+
+<!-- 추가-->
+<script src="{{ asset('js/popper.min.js') }}"></script>
+<script src="{{ asset('js/is.min.js') }}"></script>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+<script src="{{ asset('js/theme.js') }}"></script>
+
+<link href="https://fonts.googleapis.com/css2?family=Chivo:wght@300;400;700;900&amp;display=swap" rel="stylesheet">
