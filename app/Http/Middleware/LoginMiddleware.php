992284c5da67852da774d9a93f50505c2aa51320
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2021-12-30 11:23:19 +0900

    Update LoginMiddleware.php

--- app/Http/Middleware/LoginMiddleware.php
+++ app/Http/Middleware/LoginMiddleware.php
@@ -10,15 +10,16 @@ class LoginMiddleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
         // 만약 로그인이 되어 있지 않다면 로그인 페이지로 이동한다.
-        if(auth()->check()===false) {
+        if (auth()->check() === false) {
             return redirect('/login');
         }
         return $next($request);
     }
+}
