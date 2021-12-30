6f93da4eaa2c416ffbe3311a54b3ab992cb624a3
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2021-12-30 11:51:51 +0900

    Update footer.blade.php

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,3 +1,39 @@
-<!-- bootstrap js -->
-<script src="{{ asset('js/bootstrap.js') }}"></script>
-<script type="text/javascript" src="js/jquery-main.js"></script>
+<div id="page-container">
+    <div id="content-wrap">
+        <!-- all other page content -->
+
+        <footer id="footer">
+            <section class="py-0" id="contact">
+
+                <div class="container">
+                    <div class="row justify-content-lg-between min-vh-20" style="padding-top:10rem">
+                        <div class="col-12 col-lg-auto mb-3">
+                            <div class="card bg-success">
+                                <div class="card-body p-sm-4">
+                                    <h5 class="text-white">Contact to us</h5>
+                                    <p class="mb-0 text-white"><span class="text-white fs--1 fs-sm-1">jihyoung.lee90@gmail.com</span></p>
+                                    <br>
+                                    <button class="btn btn-light text-success" type="button" onclick="location.href='https://github.com/jihyoung-lee/'">
+                                        <i class="bi bi-github"></i> Git Hub
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="text-300 mb-0" />
+                    <div class="row flex-center py-5">
+                        <div class="col-12 col-sm-8 col-md-6 text-center text-md-start"> <a class="text-decoration-none" href="#"><img class="d-inline-block align-top img-fluid" src="{{asset('img/shuntian.png')}}" alt="" width="40" /></a></div>
+                        <div class="col-12 col-sm-8 col-md-6">
+                            <p class="fs--1 text-dark my-2 text-center text-md-end">&copy; ICT융합공학부 멀티미디어공학전공
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </footer>
+    </div>
+</div>
+
+<!-- ===============================================-->
+<!--    End of Main Content-->
+<!-- ===============================================-->
