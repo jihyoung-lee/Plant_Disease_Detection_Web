9e33c25641f9bf83fdd3fcee285a4159554f81d6
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2021-12-30 11:49:36 +0900

    Update navbar.blade.php

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,27 +1,15 @@
-<nav class="navbar navbar-expand-lg navbar-dark " style="background-color: #22741C;">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"><i class="bi bi-bug"></i></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 bg-light opacity-85" data-navbar-on-scroll="data-navbar-on-scroll">
+    <div class="container"><a class="navbar-brand" href="/"><img class="d-inline-block align-top img-fluid" src="{{asset('img/planting.svg')}}" alt="" width="50" /><span class="text-theme font-monospace fs-4 ps-2"></span></a>
+        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="{{route('disease.list')}}" tabindex="-1" >List</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="#" tabindex="-1" >Map</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="#" tabindex="-1" >Leaves</a>
-                </li>
+                <li class="nav-item px-2"><a class="nav-link fw-medium" href="{{route('photo.index')}}">작물 진단</a></li>
+                <li class="nav-item px-2"><a class="nav-link fw-medium" href="{{route('search.index')}}">병해충 정보</a></li>
             </ul>
-            <form class="d-flex">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-light" type="submit">Search</button>
+            <form class="d-flex"action="/list" method="POST">
+                @csrf
+                <input class="form-control me-2" name="search" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-success" type="submit">Search</button>
             </form>
         </div>
     </div>
