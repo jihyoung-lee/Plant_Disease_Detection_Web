41a8c97624d654967715a470ab189e9861fada2c
Jihyoung-Lee <jihyoung.lee90@gmail.com>
2022-10-19 16:29:18 +0900

    Update PhotoController.php
    리펙토링

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -47,49 +47,21 @@ class PhotoController extends Controller
 
         else{
 
-            $hashname =  md5_file($request->file('photo')->getRealPath());
-            $train = DB::table('train')->where('hashname',$hashname);
-            $validatedData = $request->validate([
-                   'photo' => 'mimes:jpeg,bmp,png,jpg'
-               ]);
+            list($hashname, $train, $validatedData) = $this->validateDuplicatePhoto($request);
 
 
             /* 중복데이터 검사 */
             if ($train->exists())
             {
-                $photo = Train::create([
-                    'url' =>$train->value('url'),
-                    'hashname' => $hashname,
-                    'originalname' => $train->value('originalname'),
-                    'cropName' => $train->value('cropName'),
-                    'sickNameKor' => $train->value('sickNameKor'),
-                    'confidence' => $train->value('confidence')
-                ]);
+                $photo = $this->createdata($train, $hashname);
 
             }
 
             else {
                 /* 파일 업로드 */
 
-                $url = $model.'/predict';
-                $response = Http::attach(
-                    'image', file_get_contents($validatedData['photo']), $validatedData['photo']
-                )->post($url); # 파일을 multipart 요청으로 전송
-
-                $cropName = $response->json('cropName', JSON_UNESCAPED_UNICODE);# 유니코드 한글 깨짐현상 JSON_UNESCAPED_UNICODE
-                $sickNameKor = $response->json('sickNameKor', JSON_UNESCAPED_UNICODE);
-                $confidence = $response->json('confidence');
-                $path = $request->file('photo')->store('public/'.$cropName.'_'.$sickNameKor,'s3'); # 모델 재학습을 위한 데이터 라벨링
-
-                $photo = Train::create([
-                    'url' => Storage::disk('s3')->url($path),
-                    'hashname' => $hashname,
-                    'originalname' => $request->file('photo')->getClientOriginalName(),
-                    'cropName' => $cropName,
-                    'sickNameKor' => $sickNameKor,
-                    'confidence' => $confidence
-
-                ]);
+                list($cropName, $sickNameKor, $confidence, $path) = $this->fileUpload($model, $validatedData['photo'], $request);
+                $photo = $this->getPhoto($path, $hashname, $request, $cropName, $sickNameKor, $confidence);
             }
 
 
@@ -100,5 +72,80 @@ class PhotoController extends Controller
             'status' => '정상적으로 업로드 되었습니다.'
         ]);
 
+    }
+
+    /**
+     * @param $model
+     * @param $photo1
+     * @param Request $request
+     * @return array
+     */
+    public function fileUpload($model, $photo1, Request $request): array
+    {
+        $url = $model . '/predict';
+        $response = Http::attach(
+            'image', file_get_contents($photo1), $photo1
+        )->post($url); # 파일을 multipart 요청으로 전송
+
+        $cropName = $response->json('cropName', JSON_UNESCAPED_UNICODE);# 유니코드 한글 깨짐현상 JSON_UNESCAPED_UNICODE
+        $sickNameKor = $response->json('sickNameKor', JSON_UNESCAPED_UNICODE);
+        $confidence = $response->json('confidence');
+        $path = $request->file('photo')->store('public/' . $cropName . '_' . $sickNameKor, 's3');
+        return array($cropName, $sickNameKor, $confidence, $path); # 모델 재학습을 위한 데이터 라벨링
+    }
+
+    /**
+     * @param $path
+     * @param $hashname
+     * @param Request $request
+     * @param $cropName
+     * @param $sickNameKor
+     * @param $confidence
+     * @return mixed
+     */
+    public function getPhoto($path, $hashname, Request $request, $cropName, $sickNameKor, $confidence)
+    {
+        $photo = Train::create([
+            'url' => Storage::disk('s3')->url($path),
+            'hashname' => $hashname,
+            'originalname' => $request->file('photo')->getClientOriginalName(),
+            'cropName' => $cropName,
+            'sickNameKor' => $sickNameKor,
+            'confidence' => $confidence
+
+        ]);
+        return $photo;
+    }
+
+    /**
+     * @param $train
+     * @param $hashname
+     * @return mixed
+     */
+    public function createdata($train, $hashname)
+    {
+        $photo = Train::create([
+            'url' => $train->value('url'),
+            'hashname' => $hashname,
+            'originalname' => $train->value('originalname'),
+            'cropName' => $train->value('cropName'),
+            'sickNameKor' => $train->value('sickNameKor'),
+            'confidence' => $train->value('confidence')
+        ]);
+        return $photo;
+    }
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function validateDuplicatePhoto(Request $request): array
+    {
+        $hashname = md5_file($request->file('photo')->getRealPath());
+        $train = DB::table('train')->where('hashname', $hashname);
+        $validatedData = $request->validate([
+            'photo' => 'mimes:jpeg,bmp,png,jpg'
+        ]);
+        return array($hashname, $train, $validatedData);
     }
 }
